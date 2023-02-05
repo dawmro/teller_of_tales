15a496ffa77e6f0de0058ed573e8ef457ea0973c
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-05 12:13:59 +0100

    save/load sentences to/from file

--- teller_of_tales.py
+++ teller_of_tales.py
@@ -7,12 +7,26 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from datetime import datetime
 
 import os
-
+import json
 
 
 # show step by step debug info?
 DEBUG = True
 
+
+
+def write_list(a_list, filename):
+    print("Started writing list data into a json file")
+    with open(filename, "w") as fp:
+        json.dump(a_list, fp)
+        print("Done writing JSON data into .json file")
+
+
+def read_list(filename):
+    # for reading also binary mode is important
+    with open(filename, 'rb') as fp:
+        n_list = json.load(fp)
+        return n_list
 
 
 def showTime():
@@ -44,17 +58,22 @@ def load_and_split_to_sentences(filename):
     with open(filename, "r", encoding="utf-8") as file:
         story_raw = file.read()
 
-    # remove quotes and weird symbols from story
-    story = story_raw.replace('“', '').replace('”', '').replace('——', '').replace('‘', '').replace('’', '')
+    # remove quotes from story
+    story = story_raw.replace('“', '').replace('”', '').replace('——', '')
 
     # split story into list of sentences
     story_sentences_list = sent_tokenize(story)
+    
+    write_list(story_sentences_list, "text/story_sentences_list.json")
     
     if DEBUG:
         # display story enumerating through each sentence
         for i, story_sentence in enumerate(story_sentences_list):
             print( i, story_sentence)
+        print("\n!!!!!!!!!!!!!!\nThis is last chance to make changes in story_sentences_list.json file\n!!!!!!!!!!!!!!")
         pause()
+        
+    story_sentences_list = read_list("text/story_sentences_list.json")
     
     return story_sentences_list
     
