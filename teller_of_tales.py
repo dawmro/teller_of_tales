8561e0b8ede9c7681d50e246e920b16aeabff885
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-05 11:42:35 +0100

    show current time

--- teller_of_tales.py
+++ teller_of_tales.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 
-
-
+def showTime():
+    return str("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')+" UTC]")
 
 if __name__ == "__main__":
-    pass
+
+    print(f"{showTime()}")
