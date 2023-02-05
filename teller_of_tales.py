cf0ce01261b82d7828d2a865a4854461aa1008eb
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-05 11:45:56 +0100

    create directories for output files

--- teller_of_tales.py
+++ teller_of_tales.py
@@ -1,8 +1,29 @@
 from datetime import datetime
 
+import os
+
+
 def showTime():
     return str("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')+" UTC]")
+    
+    
+    
+def createFolders():
+    
+    if not os.path.exists("text"):
+        os.makedirs("text") 
+    if not os.path.exists("audio"):
+        os.makedirs("audio")         
+    if not os.path.exists("images"):
+        os.makedirs("images")     
+    if not os.path.exists("videos"):
+        os.makedirs("videos")
+    
+    
 
 if __name__ == "__main__":
 
     print(f"{showTime()}")
+    
+    # Create directiories for text, audio, images and video files    
+    createFolders()
